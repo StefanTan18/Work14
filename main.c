d4139bf872a24115dd5b19075c34a8bf10dd55ba
Stefan Tan <stan3@stuy.edu>
2018-11-14 21:21:19 -0500

    updated work14

--- main.c
+++ main.c
@@ -4,8 +4,21 @@
 #include <string.h>
 
 char ** parse_args( char * line ) {
-  char ** a[5];
-
+  char ** a = malloc(5 * sizeof(char *));
+  int i = 0;
+  while(i < 5) {
+    a[i] = strsep(&line, " ");
+    i++;
+  }
+  return a;
 }
 
+int main() {
   
+  char line[100] = "ls -a -l";
+  char ** args = parse_args( line );
+  execvp(args[0], args);
+
+  return 0;
+}
+
